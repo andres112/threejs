174bdf78f3099e0e50d608fe511e23a107d48081
Andres Felipe Dorado <andres.dorado90@gmail.com>
2024-06-01 14:23:59 +0200

    chore: js base camera controls

--- Cameras/src/script.js
+++ Cameras/src/script.js
@@ -6,6 +6,19 @@ import * as THREE from 'three';
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
 
+/**
+ * Cursor
+ */
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener('mousemove', (event) => {
+ cursor.x = event.clientX / sizes.width - 0.5;
+ cursor.y = -(event.clientY / sizes.height - 0.5);
+});
+
 // Sizes
 const sizes = {
   width: window.innerWidth,
@@ -17,19 +30,19 @@ const scene = new THREE.Scene();
 
 // Object
 const mesh = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-  new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true})
+  new THREE.BoxGeometry(1, 1, 1, 50, 5, 5),
+  new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
 );
 scene.add(mesh);
 
 // Cameras
-// const camera = new THREE.PerspectiveCamera(70, sizes.width / sizes.height, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(70, sizes.width / sizes.height, 0.1, 100);
 
 // solve the problem of the object being clipped by the camera
-const aspectRatio = sizes.width / sizes.height;
-const camera = new THREE.OrthographicCamera(-aspectRatio, aspectRatio, 1, -1, 0.1, 100);
-camera.position.x = 2;
-camera.position.y = -2;
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(-aspectRatio, aspectRatio, 1, -1, 0.1, 100);
+// camera.position.x = 2;
+// camera.position.y = 2;
 camera.position.z = 2;
 camera.lookAt(mesh.position);
 scene.add(camera);
@@ -47,7 +60,15 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // Update objects
-  mesh.rotation.y = elapsedTime;
+  // mesh.rotation.z = elapsedTime;
+  // mesh.rotation.y = elapsedTime;
+  mesh.rotation.x = elapsedTime;
+
+  // Update camera
+  camera.position.x = Math.cos(cursor.x * Math.PI * 2) * 3;
+  camera.position.z = Math.sin(cursor.x * Math.PI * 2) * 3;
+  camera.position.y = cursor.y * 5;
+  camera.lookAt(mesh.position);
 
   // Render
   renderer.render(scene, camera);

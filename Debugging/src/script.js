8946df7fd4b35da9aaa9923840a057beb966294b
Andres Felipe Dorado <andres.dorado90@gmail.com>
2024-06-08 15:41:25 +0200

    chore: add some tweaks for debugging

--- Debugging/src/script.js
+++ Debugging/src/script.js
@@ -1,8 +1,11 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import { SlowMo } from "gsap/EasePack";
 import GUI from 'lil-gui'
 
+
+gsap.registerPlugin(SlowMo);
 
 /**
  * Debug
@@ -21,10 +24,23 @@ const scene = new THREE.Scene()
 /**
  * Object
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
-const material = new THREE.MeshBasicMaterial({ color: '#ff0000' })
+const geometry = new THREE.SphereGeometry(1, 32, 16)
+const material = new THREE.MeshBasicMaterial({ color: '#ff0000', wireframe: true})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+// Debug cube
+gui.add(mesh.position, 'y').min(-3).max(3).step(0.01).name('elevation')
+gui.add(mesh.position, 'x').min(-3).max(3).step(0.01).name('azimuth')
+gui.add(mesh, 'visible')
+gui.add(mesh.material, 'wireframe')
+gui.addColor(mesh.material, 'color')
+// Custom scale controller
+let scaleValue = 1;
+const scaleController = gui.add({ scale: scaleValue }, 'scale').min(0.5).max(3).step(0.01).name('scale');
+scaleController.onChange(function (value) {
+  mesh.scale.set(value, value, value);
+});
 
 /**
  * Sizes
@@ -56,7 +72,7 @@ window.addEventListener('resize', () =>
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
 camera.position.y = 1
-camera.position.z = 2
+camera.position.z = 5
 scene.add(camera)
 
 // Controls

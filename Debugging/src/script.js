a8bb138ae35319009d55b33dca1617d3fd8546c4
Andres Felipe Dorado <andres.dorado90@gmail.com>
2024-06-08 15:56:35 +0200

    chore: add some animation with green sock

--- Debugging/src/script.js
+++ Debugging/src/script.js
@@ -1,43 +1,47 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import gsap from 'gsap'
-import { SlowMo } from "gsap/EasePack";
-import GUI from 'lil-gui'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import gsap from 'gsap';
+import { CustomEase } from 'gsap/CustomEase';
+import GUI from 'lil-gui';
 
-
-gsap.registerPlugin(SlowMo);
+gsap.registerPlugin(CustomEase);
 
 /**
  * Debug
  */
-const gui = new GUI()
+const gui = new GUI();
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Object
  */
-const geometry = new THREE.SphereGeometry(1, 32, 16)
-const material = new THREE.MeshBasicMaterial({ color: '#ff0000', wireframe: true})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const geometry = new THREE.SphereGeometry(1, 32, 16);
+const material = new THREE.MeshBasicMaterial({ color: '#ff0000', wireframe: true });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 // Debug cube
-gui.add(mesh.position, 'y').min(-3).max(3).step(0.01).name('elevation')
-gui.add(mesh.position, 'x').min(-3).max(3).step(0.01).name('azimuth')
-gui.add(mesh, 'visible')
-gui.add(mesh.material, 'wireframe')
-gui.addColor(mesh.material, 'color')
+gui.add(mesh.position, 'y').min(-3).max(3).step(0.01).name('elevation');
+gui.add(mesh.position, 'x').min(-3).max(3).step(0.01).name('azimuth');
+gui.add(mesh, 'visible');
+gui.add(mesh.material, 'wireframe');
+gui.addColor(mesh.material, 'color');
 // Custom scale controller
 let scaleValue = 1;
-const scaleController = gui.add({ scale: scaleValue }, 'scale').min(0.5).max(3).step(0.01).name('scale');
+const scaleController = gui
+  .add({ scale: scaleValue }, 'scale')
+  .min(0.5)
+  .max(3)
+  .step(0.01)
+  .name('scale');
 scaleController.onChange(function (value) {
   mesh.scale.set(value, value, value);
 });
@@ -46,65 +50,72 @@ scaleController.onChange(function (value) {
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 5
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 5;
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update();
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera);
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
 
-tick()
+tick();
+
+CustomEase.create(
+  'hop',
+  'M0,0 C0,0 0.056,0.442 0.175,0.442 0.294,0.442 0.332,0 0.332,0 0.332,0 0.414,1 0.671,1 0.991,1 1,0 1,0'
+);
+const timeline = gsap.timeline({ repeat: -1 });
+timeline
+  .to(mesh.position, { duration: 2.5, y: 4, delay:1, ease: 'bounce.in' })
+  .to(mesh.position, { duration: 3, y: 0, ease: 'bounce.out' });

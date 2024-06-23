dcd7836540501717e9e5913c200ba065664ed68f
Andres Felipe Dorado <andres.dorado90@gmail.com>
2024-06-23 14:33:26 +0200

    chore: creation of random geometries to put around the text

--- 8-3dtext/src/script.js
+++ 8-3dtext/src/script.js
@@ -9,6 +9,10 @@ import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
  */
 // Debug
 const gui = new GUI();
+const debugObject = {
+  matcap: 3,
+};
+const matcapFolder = gui.addFolder('Matcaps');
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
@@ -24,35 +28,79 @@ scene.add(axesHelper);
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const matcapTextures = [];
+for (let i = 0; i < 9; i++) {
+  matcapTextures.push(textureLoader.load(`/textures/matcaps/${i}.png`));
+  //   matcapTextures[i].colorSpace = THREE.sRGBEncoding;
+}
 
 /**
  * Fonts
  */
 const fontLoader = new FontLoader();
+const BEVEL_SIZE = 0.02;
 fontLoader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
-  const textGeometry = new TextGeometry('Andres', {
+  const textGeometry = new TextGeometry('Andres Dorado', {
     font,
     size: 0.3,
     depth: 0.2,
-    curveSegments: 5,
+    curveSegments: 5, // defines the resolution of the curve
     bevelEnabled: true,
     bevelThickness: 0.03,
-    bevelSize: 0.02,
+    bevelSize: BEVEL_SIZE,
     bevelOffset: 0,
-    bevelSegments: 4,
+    bevelSegments: 4, // defines the resolution of the bevel curve
   });
-  const textMaterial = new THREE.MeshNormalMaterial();
-  textMaterial.wireframe = true;
+  //   // compute bounding box for getting the size of the text
+  //   textGeometry.computeBoundingBox();
+  //   console.log(textGeometry.boundingBox);
+
+  //   // center the text
+  //   textGeometry.translate(
+  //     (textGeometry.boundingBox.max.x - BEVEL_SIZE) * -0.5,
+  //     (textGeometry.boundingBox.max.y - BEVEL_SIZE) * -0.5,
+  //     (textGeometry.boundingBox.max.z - BEVEL_SIZE) * -0.5
+  //   );
+
+  // center the text
+  textGeometry.center();
+
+  const textMaterial = new THREE.MeshMatcapMaterial();
+  textMaterial.wireframe = false;
+  textMaterial.matcap = matcapTextures[3];
   const text = new THREE.Mesh(textGeometry, textMaterial);
   scene.add(text);
+
+  // Debug
+  matcapFolder
+    .add(debugObject, 'matcap')
+    .min(1)
+    .max(matcapTextures.length - 1)
+    .step(1)
+    .name('Matcap')
+    .onChange(() => {
+      textMaterial.dispose();
+      textMaterial.matcap = matcapTextures[debugObject.matcap];
+    });
 });
 
 /**
  * Object
  */
-// const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial());
 
-// scene.add(cube);
+for (let i = 0; i < 100; i++) {
+  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
+  const donutMaterial = new THREE.MeshMatcapMaterial();
+  donutMaterial.matcap = matcapTextures[Math.floor(Math.random() * matcapTextures.length + 1)];
+  const donut = new THREE.Mesh(donutGeometry, donutMaterial);
+  donut.position.set(
+    (Math.random() - 0.5) * 12,
+    (Math.random() - 0.5) * 12,
+    (Math.random() - 0.5) * 12
+  );
+  donut.rotation.set(Math.random() * Math.PI, Math.random() * Math.PI, Math.random() * Math.PI);
+  scene.add(donut);
+}
 
 /**
  * Sizes

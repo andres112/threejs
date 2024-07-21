4c0a587999cab78b9963bb73858370565da09275
Andres Felipe Dorado <andres.dorado90@gmail.com>
2024-07-21 15:31:35 +0200

    feat: roof created and added to the house

--- 11-Project-Haunted_House/src/Objects/house.ts
+++ 11-Project-Haunted_House/src/Objects/house.ts
@@ -1,22 +1,45 @@
-import { Mesh, SphereGeometry, MeshStandardMaterial, Group, BoxGeometry } from 'three';
+import {
+  Mesh,
+  SphereGeometry,
+  MeshStandardMaterial,
+  Group,
+  BoxGeometry,
+  ConeGeometry,
+} from 'three';
+
+const WALLS_DIMENSION = { width: 4, height: 2.5, depth: 4 };
+const ROOF_DIMENSION = { radius: 3.3, height: 1.5 };
 
 export class House extends Group {
-  private walls= new Mesh();
+  private walls = new Mesh();
+  private roof = new Mesh();
 
   constructor() {
     super();
     this.buildWalls();
+    this.buildRoof();
     this.buildHouse();
   }
 
   private buildHouse(): void {
     this.add(this.walls as Mesh);
+    this.add(this.roof as Mesh);
   }
 
   private buildWalls(): void {
     this.walls = new Mesh(
-      new BoxGeometry(4, 2.5, 4), 
+      new BoxGeometry(WALLS_DIMENSION.width, WALLS_DIMENSION.height, WALLS_DIMENSION.depth),
       new MeshStandardMaterial()
     );
+    this.walls.position.y = WALLS_DIMENSION.height * 0.5;
+  }
+
+  private buildRoof(): void {
+    this.roof = new Mesh(
+      new ConeGeometry(ROOF_DIMENSION.radius, ROOF_DIMENSION.height, 4),
+      new MeshStandardMaterial()
+    );
+    this.roof.position.y = WALLS_DIMENSION.height * 1.25;
+    this.roof.rotation.y = Math.PI * 0.25
   }
 }

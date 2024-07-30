722848d9fa0931f50353045a76d255e8562bbf61
Andres Felipe Dorado <andres.dorado90@gmail.com>
2024-07-30 20:38:46 +0200

    refactor: Update bush texture and material in Bush class

--- 11-Project-Haunted_House/src/Objects/bush.ts
+++ 11-Project-Haunted_House/src/Objects/bush.ts
@@ -1,8 +1,16 @@
 import { Mesh, MeshStandardMaterial, SphereGeometry, Vector3 } from 'three';
+import { CustomTexture } from '../textures/main';
 
 export class Bush extends Mesh {
   constructor(position: Vector3, scale: number = 1) {
-    super(new SphereGeometry(1, 16, 16), new MeshStandardMaterial({ color: 0x00ff00 }));
+    super(new SphereGeometry(1, 16, 16), new MeshStandardMaterial({ 
+      map: CustomTexture.bush.color,
+      aoMap: CustomTexture.bush.ao,
+      normalMap: CustomTexture.bush.normal,
+      roughnessMap: CustomTexture.bush.roughness,
+      displacementMap: CustomTexture.bush.height,
+      displacementScale: 0.5,
+     }));
     this.position.set(position.x, position.y, position.z);
     this.scale.setScalar(scale);
   }
--- 11-Project-Haunted_House/src/textures/main.ts
+++ 11-Project-Haunted_House/src/textures/main.ts
@@ -19,10 +19,20 @@ const repeatTexture = (texture: Texture, repeat: Vector2): Texture => {
 
 export class CustomTexture {
   public static readonly floor: { [k: string]: Texture } = {
-    alpha: loadTexture('../textures/floor/alpha.jpg', ),
+    alpha: loadTexture('../textures/floor/alpha.jpg'),
     color: loadTexture('../textures/floor/coast_sand_rocks_02_diff_1k.jpg', new Vector2(8, 8)),
     normal: loadTexture('../textures/floor/coast_sand_rocks_02_nor_gl_1k.png', new Vector2(8, 8)),
-    displacement: loadTexture('../textures/floor/coast_sand_rocks_02_disp_1k.png', new Vector2(8, 8)),
+    displacement: loadTexture(
+      '../textures/floor/coast_sand_rocks_02_disp_1k.png',
+      new Vector2(8, 8)
+    ),
     arm: loadTexture('../textures/floor/coast_sand_rocks_02_arm_1k.png', new Vector2(8, 8)),
+  };
+  public static readonly bush: { [k: string]: Texture } = {
+    ao: loadTexture('../textures/bush/Hedge_001_AmbientOcclusion.jpg', new Vector2(4, 4)),
+    color: loadTexture('../textures/bush/Hedge_001_BaseColor.jpg', new Vector2(4, 4)),
+    normal: loadTexture('../textures/bush/Hedge_001_Normal.jpg', new Vector2(4, 4)),
+    roughness: loadTexture('../textures/bush/Hedge_001_Roughness.jpg', new Vector2(4, 4)),
+    height: loadTexture('../textures/bush/Hedge_001_Height.png', new Vector2(4, 4)),
   };
 }

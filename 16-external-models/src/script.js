ece9b4e553a51d67a67beddc928046d9061e1b87
Andres Felipe Dorado <andres.dorado90@gmail.com>
2024-09-09 11:41:53 +0200

    refactor: Remove unused Fox model and add Biomech Mutant models

--- 16-external-models/src/script.js
+++ 16-external-models/src/script.js
@@ -1,160 +1,241 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import GUI from 'lil-gui'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import GUI from 'lil-gui';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 /**
  * Base
  */
 // Debug
-const gui = new GUI()
+const gui = new GUI();
+const helperFolder = gui.addFolder('Helpers');
+const debugObject = {};
+debugObject.toggleShadows = true;
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
-const axesHelper = new THREE.AxesHelper(2)
-scene.add(axesHelper)
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
+
+helperFolder.add(axesHelper, 'visible').name('Axes Helper');
 
 /**
  * External Model
  */
-const gltfLoader = new GLTFLoader()
+const gltfLoader = new GLTFLoader();
+// FlightHelmet model
 gltfLoader.load(
-    '/models/FlightHelmet/glTF/FlightHelmet.gltf',
-    (gltf) =>
-    {
-        console.log(gltf)
+  '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+  (gltf) => {
+    console.log('FlightHelmet', gltf);
 
-        // the model is in the gltf.scene property
-        gltf.scene.scale.setScalar(5)
+    // the model is in the gltf.scene property
+    gltf.scene.scale.setScalar(3);
+    gltf.scene.position.set(2, 0, 0);
+    // cast shadows
+    add3DModelShadow(gltf);
 
-        /**Different ways to add the model */
-        // Filtering scene.children
-        // gltf.scene.children.forEach(child =>
-        // {
-        //     if(child.name.toLowerCase().includes('metal'))
-        //     {
-        //         child.visible = false
-        //     }
-        // })
+    /**Different ways to add the model */
+    // Filtering scene.children
+    // gltf.scene.children.forEach(child =>
+    // {
+    //     if(child.name.toLowerCase().includes('metal'))
+    //     {
+    //         child.visible = false
+    //     }
+    // })
+    scene.add(gltf.scene);
+  },
+  () => {
+    // onProgress callback
+    console.log('FlightHelmet loading...');
+  }
+);
 
-        //
+// Duck model
+gltfLoader.load('/models/Duck/glTF/Duck.gltf', (gltf) => {
+  console.log('Duck', gltf);
 
-        scene.add(gltf.scene)
+  // the model is in the gltf.scene property
+  gltf.scene.scale.setScalar(0.5);
+  gltf.scene.rotateY(Math.PI * -0.75);
+  gltf.scene.position.set(-2, 0, 0);
+  
+  add3DModelShadow(gltf);
+
+  scene.add(gltf.scene);
+});
+
+// Biomechanical Mutant model 1
+gltfLoader.load('/models/Biomech_Mutant/Skin_1/Biomech_Mutant_Skin_1.gltf', (gltf) => {
+  console.log('Biomech_Mutant_Skin_1', gltf);
+
+  // the model is in the gltf.scene property
+  gltf.scene.scale.setScalar(2.5);
+  gltf.scene.position.set(0, 0, 0);
+
+  // cast shadows
+  add3DModelShadow(gltf);
+
+  scene.add(gltf.scene);
+});
+
+// Biomechanical Mutant model 2
+gltfLoader.load('/models/Biomech_Mutant/Skin_2/Biomech_Mutant_Skin_2.gltf', (gltf) => {
+  console.log('Biomech_Mutant_Skin_2', gltf);
+
+  // the model is in the gltf.scene property
+  gltf.scene.scale.setScalar(2.5);
+  gltf.scene.position.set(0, 0, -3);
+
+  // cast shadows
+  add3DModelShadow(gltf);
+
+  scene.add(gltf.scene);
+});
+
+/**
+ * Shadows
+ */
+const add3DModelShadow = (object) => {
+  object.scene.traverse((child) => {
+    if (child instanceof THREE.Mesh) {
+      child.castShadow = true;
     }
-)
+  });
+};
+
+// add gui to hide shadows
+helperFolder
+  .add(debugObject, 'toggleShadows')
+  .name('Toggle Shadows')
+  .onChange(() => {
+    scene.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        child.castShadow = !child.castShadow;
+        child.receiveShadow = !child.receiveShadow;
+      }
+    });
+  });
 
 /**
  * Floor
  */
 const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(20, 20),
-    new THREE.MeshStandardMaterial({
-        color: '#444444',
-        metalness: 0,
-        roughness: 0.5
-    })
-)
-floor.receiveShadow = true
-floor.rotation.x = - Math.PI * 0.5
-scene.add(floor)
+  new THREE.PlaneGeometry(20, 20),
+  new THREE.MeshStandardMaterial({
+    color: '#444444',
+    metalness: 0,
+    roughness: 0.5,
+  })
+);
+floor.receiveShadow = true;
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
 
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 2.4)
-scene.add(ambientLight)
+const ambientLight = new THREE.AmbientLight(0xffffff, 2.4);
+scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 3)
-directionalLight.castShadow = true
-directionalLight.shadow.mapSize.set(1024, 1024)
-directionalLight.shadow.camera.far = 20
-directionalLight.shadow.camera.left = - 15
-directionalLight.shadow.camera.top = 10
-directionalLight.shadow.camera.right = 15
-directionalLight.shadow.camera.bottom = - 7
-directionalLight.position.set(5, 5, 5)
-scene.add(directionalLight)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.far = 20;
+directionalLight.shadow.camera.left = -15;
+directionalLight.shadow.camera.top = 10;
+directionalLight.shadow.camera.right = 15;
+directionalLight.shadow.camera.bottom = -7;
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
 
-const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
-scene.add(directionalLightHelper)
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2);
+scene.add(directionalLightHelper);
 
-const helper = new THREE.CameraHelper(directionalLight.shadow.camera)
-scene.add(helper)
+helperFolder.add(directionalLightHelper, 'visible').name('Directional Light Helper');
+
+const helper = new THREE.CameraHelper(directionalLight.shadow.camera);
+scene.add(helper);
+
+helperFolder.add(helper, 'visible').name('Camera Helper');
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(3, 5, 4)
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.set(3, 5, 4);
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.target.set(0, 0.75, 0)
-controls.enableDamping = true
-controls.minDistance = 1
-controls.maxDistance = 20
-controls.maxPolarAngle = Math.PI * 0.5
+const controls = new OrbitControls(camera, canvas);
+controls.target.set(0, 0.75, 0);
+controls.enableDamping = true;
+controls.minDistance = 1;
+controls.maxDistance = 20;
+controls.maxPolarAngle = Math.PI * 0.5;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+});
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
-let previousTime = 0
+const clock = new THREE.Clock();
+let previousTime = 0;
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - previousTime
-    previousTime = elapsedTime
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
 
-    // Update controls
-    controls.update()
+  // Rotate directional light around scene slowly
+  directionalLight.position.x = Math.sin(elapsedTime * 0.1) * 5;
+  directionalLight.position.z = Math.cos(elapsedTime * 0.1) * 5;
 
-    // Render
-    renderer.render(scene, camera)
+  // Update controls
+  controls.update();
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  // Render
+  renderer.render(scene, camera);
 
-tick()
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();

5aed4b134e7aec9a5c5cade5541bf7b18486b131
Andres Felipe Dorado <andres.dorado90@gmail.com>
2024-10-29 14:02:21 +0100

    refactor: Update tone mapping options in renderer

--- 19-realistic-render/src/script.js
+++ 19-realistic-render/src/script.js
@@ -116,6 +116,17 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+// Tone mapping
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+
+gui.add(renderer, 'toneMapping', {
+    None: THREE.NoToneMapping,
+    Linear: THREE.LinearToneMapping,
+    Reinhard: THREE.ReinhardToneMapping,
+    Cineon: THREE.CineonToneMapping,
+    ACESFilmic: THREE.ACESFilmicToneMapping,
+})
+
 /**
  * Animate
  */

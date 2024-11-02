53891df5159967964dabe042c998bcb998505b4c
Andres Felipe Dorado <andres.dorado90@gmail.com>
2024-11-02 16:46:41 +0100

    feat: enhance Environment class to improve sunlight setup and add shadow camera helper

--- 20-big-projects/src/World/Environment.ts
+++ 20-big-projects/src/World/Environment.ts
@@ -5,21 +5,31 @@ import Helper from '@/gui/Helper';
 
 export default class Environment {
   private gui: GUI;
+  private sunlight!: THREE.DirectionalLight;
 
   constructor() {
-    this.gui= Helper.addFolder('Environment');
+    this.gui = Helper.addFolder('Lighting');
     this.setSunlight();
     console.info('Environment initialized');
   }
 
   private setSunlight() {
-    const sunlight = new THREE.DirectionalLight(0xffffff, 1);
-    sunlight.position.set(2, 2, 2);
-    App.instance.scene.add(sunlight);
-    this.gui.add(sunlight, 'intensity', 0, 5, 0.01).name('Sunlight intensity');
+    this.sunlight = new THREE.DirectionalLight('#ffffff', 4);
+    this.sunlight.castShadow = true;
+    this.sunlight.shadow.camera.far = 10;
+    this.sunlight.shadow.mapSize.set(1024, 1024);
+    this.sunlight.shadow.normalBias = 0.05;
+    this.sunlight.position.set(3.5, 2, -1.25);
+    App.instance.scene.add(this.sunlight);
+    this.gui.add(this.sunlight, 'intensity', 0, 5, 0.01).name('Sunlight intensity');
 
-    const sunlightHelper = new THREE.DirectionalLightHelper(sunlight, 0.2);
+    const sunlightHelper = new THREE.DirectionalLightHelper(this.sunlight, 0.2);
     App.instance.scene.add(sunlightHelper);
     this.gui.add(sunlightHelper, 'visible').name('Sunlight helper');
+
+    // light shadow camera helper
+    const shadowCameraHelper = new THREE.CameraHelper(this.sunlight.shadow.camera);
+    App.instance.scene.add(shadowCameraHelper);
+    this.gui.add(shadowCameraHelper, 'visible').name('Shadow camera helper');
   }
 }

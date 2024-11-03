3440e6e137bd4fdd0c4454c4f02890679437e735
Andres Felipe Dorado <andres.dorado90@gmail.com>
2024-11-03 12:13:30 +0100

    feat: enhance Character class with animation support and update model loading

--- 20-big-projects/src/World/Character.ts
+++ 20-big-projects/src/World/Character.ts
@@ -1,20 +1,36 @@
-import App from "@/app/App";
+import App from '@/app/App';
+import { IAnimation } from '@/resources/Interfaces';
+import * as THREE from 'three';
+import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 
 export default class Character {
-    constructor() {
-        // setup
-        this.setModel();
-        console.info('Character initialized');
-    }
+  private model!: THREE.Group<THREE.Object3DEventMap>;
+  private animation: IAnimation = {} as IAnimation;
 
-    private setModel() {
+  constructor() {
+    // setup
+    this.setModel();
+    this.setAnimation();
+    console.info('Character initialized');
+  }
 
-        // Load character
-        const gltfLoader = App.instance.resources.items['goblinModel'];
-        const model = gltfLoader.scene;
-        model.scale.set(0.02, 0.02, 0.02);
-        model.position.set(0, 0, 0);
-        model.rotation.x = -Math.PI * 0.5;
-        App.instance.scene.add(model);
-    }
+  private setModel() {
+    // Load character
+    const gltfLoader = App.instance.resources.items['goblinModel'] as GLTF;
+    this.model = gltfLoader.scene;
+    this.model.scale.set(0.02, 0.02, 0.02);
+    this.model.position.set(0, 0, 0);
+    this.model.rotation.x = -Math.PI * 0.5;
+    App.instance.scene.add(this.model);
+  }
+
+  private setAnimation() {
+    this.animation.mixer = new THREE.AnimationMixer(this.model);
+    this.animation.action = this.animation.mixer.clipAction(
+      (App.instance.resources.items['goblinModel'] as GLTF).animations[0]
+    );
+    this.animation.action.play();
+
+    console.log(this.animation);
+  }
 }
--- 20-big-projects/src/resources/Interfaces.ts
+++ 20-big-projects/src/resources/Interfaces.ts
@@ -1,5 +1,12 @@
+import { AnimationMixer, AnimationAction } from 'three';
+
 export interface ISource {
   name: string;
   type: 'texture' | 'cubeTexture' | 'gltf';
   path: string | string[];
+}
+
+export interface IAnimation {
+  mixer: AnimationMixer;
+  action?: AnimationAction;
 }

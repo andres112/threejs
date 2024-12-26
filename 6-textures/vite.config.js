2a6302f98cf8b5892becb8bfdd222efbfe4b3f85
Andres Felipe Dorado <andres.dorado90@gmail.com>
2024-12-26 16:33:56 +0100

    feat: replace old CSS file with new one and update asset paths in HTML and Vite configuration

--- 6-textures/vite.config.js
+++ 6-textures/vite.config.js
@@ -3,7 +3,7 @@ import restart from 'vite-plugin-restart'
 export default {
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
-    base: './', // Base URL when the app is running in a subdirectory
+    base: '/threejs/6-textures/dist/', // Base URL when the app is running in a subdirectory
     server:
     {
         host: true, // Open to local network and display URL
@@ -15,17 +15,6 @@ export default {
         emptyOutDir: true, // Empty the folder first
         sourcemap: true, // Add sourcemap
         assetsInlineLimit: 0, // Ensure all assets are written to files, not inlined
-        rollupOptions: {
-            output: {
-                assetFileNames: (assetInfo) => {
-                    // Ensure assets (like textures) are placed correctly
-                    if (/textures/.test(assetInfo.name)) {
-                        return 'textures/[name][extname]';
-                    }
-                    return 'assets/[name][extname]';
-                },
-            },
-        },
     },
     plugins:
     [

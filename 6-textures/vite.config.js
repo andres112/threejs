2089a1f914a13395b2f8259ec097e190e04b15c2
Andres Felipe Dorado <andres.dorado90@gmail.com>
2024-12-26 16:35:20 +0100

    feat: add new texture assets and update Vite configuration for base URL

--- 6-textures/vite.config.js
+++ 6-textures/vite.config.js
@@ -3,7 +3,7 @@ import restart from 'vite-plugin-restart'
 export default {
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
-    base: '/threejs/6-textures/dist/', // Base URL when the app is running in a subdirectory
+    base: './', // Base URL when the app is running in a subdirectory
     server:
     {
         host: true, // Open to local network and display URL

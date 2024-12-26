2a57bc74a7b4058363566dba90b781e7620deecd
Andres Felipe Dorado <andres.dorado90@gmail.com>
2024-12-26 17:41:55 +0100

    feat: update asset paths in HTML and TypeScript files to use relative paths

--- 11-Project-Haunted_House/src/textures/textureFactory.ts
+++ 11-Project-Haunted_House/src/textures/textureFactory.ts
@@ -4,19 +4,19 @@ import { loadTexture } from './textureUtils';
 export class TextureFactory {
   public static createFloorTextures(): { [key: string]: Texture } {
     return {
-      alpha: loadTexture('../textures/floor/alpha.jpg'),
-      color: loadTexture('../textures/floor/coast_sand_rocks_02_diff_1k.webp', {
+      alpha: loadTexture('textures/floor/alpha.jpg'),
+      color: loadTexture('textures/floor/coast_sand_rocks_02_diff_1k.webp', {
         repeat: new Vector2(8, 8),
         colorSpace: true,
         magFilter: true,
       }),
-      normal: loadTexture('../textures/floor/coast_sand_rocks_02_nor_gl_1k.webp', {
+      normal: loadTexture('textures/floor/coast_sand_rocks_02_nor_gl_1k.webp', {
         repeat: new Vector2(8, 8),
       }),
-      displacement: loadTexture('../textures/floor/coast_sand_rocks_02_disp_1k.webp', {
+      displacement: loadTexture('textures/floor/coast_sand_rocks_02_disp_1k.webp', {
         repeat: new Vector2(8, 8),
       }),
-      arm: loadTexture('../textures/floor/coast_sand_rocks_02_arm_1k.webp', {
+      arm: loadTexture('textures/floor/coast_sand_rocks_02_arm_1k.webp', {
         repeat: new Vector2(8, 8),
       }),
     };
@@ -24,30 +24,30 @@ export class TextureFactory {
 
   public static createHouseWallsTextures(): { [key: string]: Texture } {
     return {
-      arm: loadTexture('../textures/wall/castle_brick_broken_06_arm_1k.webp'),
-      color: loadTexture('../textures/wall/castle_brick_broken_06_diff_1k.webp', {
+      arm: loadTexture('textures/wall/castle_brick_broken_06_arm_1k.webp'),
+      color: loadTexture('textures/wall/castle_brick_broken_06_diff_1k.webp', {
         colorSpace: true,
         magFilter: true,
       }),
-      normal: loadTexture('../textures/wall/castle_brick_broken_06_nor_gl_1k.webp'),
-      displacement: loadTexture('../textures/wall/castle_brick_broken_06_disp_1k.webp'),
+      normal: loadTexture('textures/wall/castle_brick_broken_06_nor_gl_1k.webp'),
+      displacement: loadTexture('textures/wall/castle_brick_broken_06_disp_1k.webp'),
     };
   }
 
   public static createHouseChimneyTextures(): { [key: string]: Texture } {
     return {
-      arm: loadTexture('../textures/chimney/rock_wall_10_arm_1k.webp', {
+      arm: loadTexture('textures/chimney/rock_wall_10_arm_1k.webp', {
         repeat: new Vector2(1, 4),
       }),
-      color: loadTexture('../textures/chimney/rock_wall_10_diff_1k.webp', {
+      color: loadTexture('textures/chimney/rock_wall_10_diff_1k.webp', {
         repeat: new Vector2(1, 4),
         angle: Math.PI * 0.5,
         colorSpace: true,
       }),
-      normal: loadTexture('../textures/chimney/rock_wall_10_nor_gl_1k.webp', {
+      normal: loadTexture('textures/chimney/rock_wall_10_nor_gl_1k.webp', {
         repeat: new Vector2(1, 4),
       }),
-      displacement: loadTexture('../textures/chimney/rock_wall_10_disp_1k.webp', {
+      displacement: loadTexture('textures/chimney/rock_wall_10_disp_1k.webp', {
         repeat: new Vector2(1, 4),
       }),
     };
@@ -55,16 +55,16 @@ export class TextureFactory {
 
   public static createHouseRoofTextures(): { [key: string]: Texture } {
     return {
-      arm: loadTexture('../textures/roof/roof_07_arm_1k.webp', { repeat: new Vector2(3, 1) }),
-      color: loadTexture('../textures/roof/roof_07_diff_1k.webp', {
+      arm: loadTexture('textures/roof/roof_07_arm_1k.webp', { repeat: new Vector2(3, 1) }),
+      color: loadTexture('textures/roof/roof_07_diff_1k.webp', {
         colorSpace: true,
         repeat: new Vector2(3, 1),
         magFilter: true,
       }),
-      normal: loadTexture('../textures/roof/roof_07_nor_gl_1k.webp', {
+      normal: loadTexture('textures/roof/roof_07_nor_gl_1k.webp', {
         repeat: new Vector2(3, 1),
       }),
-      displacement: loadTexture('../textures/roof/roof_07_disp_1k.webp', {
+      displacement: loadTexture('textures/roof/roof_07_disp_1k.webp', {
         repeat: new Vector2(3, 1),
       }),
     };
@@ -72,50 +72,50 @@ export class TextureFactory {
 
   public static createHouseDoorTextures(): { [key: string]: Texture } {
     return {
-      ao: loadTexture('../textures/door/Wood_Door_002_ambientOcclusion.webp'),
-      color: loadTexture('../textures/door/Wood_Door_002_basecolor.webp', {
+      ao: loadTexture('textures/door/Wood_Door_002_ambientOcclusion.webp'),
+      color: loadTexture('textures/door/Wood_Door_002_basecolor.webp', {
         colorSpace: true,
       }),
-      normal: loadTexture('../textures/door/Wood_Door_002_normal.webp'),
-      displacement: loadTexture('../textures/door/Wood_Door_002_height.webp'),
-      metallic: loadTexture('../textures/door/Wood_Door_002_metallic.webp'),
-      roughness: loadTexture('../textures/door/Wood_Door_002_roughness.webp'),
+      normal: loadTexture('textures/door/Wood_Door_002_normal.webp'),
+      displacement: loadTexture('textures/door/Wood_Door_002_height.webp'),
+      metallic: loadTexture('textures/door/Wood_Door_002_metallic.webp'),
+      roughness: loadTexture('textures/door/Wood_Door_002_roughness.webp'),
     };
   }
 
   public static createBushTextures(): { [key: string]: Texture } {
     return {
-      ao: loadTexture('../textures/bush/Hedge_001_AmbientOcclusion.webp', {
+      ao: loadTexture('textures/bush/Hedge_001_AmbientOcclusion.webp', {
         repeat: new Vector2(3, 3),
       }),
-      color: loadTexture('../textures/bush/Hedge_001_BaseColor.webp', {
+      color: loadTexture('textures/bush/Hedge_001_BaseColor.webp', {
         repeat: new Vector2(3, 3),
         colorSpace: true,
         magFilter: true,
       }),
-      normal: loadTexture('../textures/bush/Hedge_001_Normal.webp', { repeat: new Vector2(3, 3) }),
-      roughness: loadTexture('../textures/bush/Hedge_001_Roughness.webp', {
+      normal: loadTexture('textures/bush/Hedge_001_Normal.webp', { repeat: new Vector2(3, 3) }),
+      roughness: loadTexture('textures/bush/Hedge_001_Roughness.webp', {
         repeat: new Vector2(3, 3),
       }),
-      height: loadTexture('../textures/bush/Hedge_001_Height.webp', { repeat: new Vector2(3, 3) }),
+      height: loadTexture('textures/bush/Hedge_001_Height.webp', { repeat: new Vector2(3, 3) }),
     };
   }
 
   public static createGraveTextures(): { [key: string]: Texture } {
     return {
-      arm: loadTexture('../textures/grave/plastered_stone_wall_arm_1k.webp'),
-      color: loadTexture('../textures/grave/plastered_stone_wall_diff_1k.webp', {
+      arm: loadTexture('textures/grave/plastered_stone_wall_arm_1k.webp'),
+      color: loadTexture('textures/grave/plastered_stone_wall_diff_1k.webp', {
         colorSpace: true,
         magFilter: true,
       }),
-      normal: loadTexture('../textures/grave/plastered_stone_wall_nor_gl_1k.webp'),
-      displacement: loadTexture('../textures/grave/plastered_stone_wall_disp_1k.webp'),
+      normal: loadTexture('textures/grave/plastered_stone_wall_nor_gl_1k.webp'),
+      displacement: loadTexture('textures/grave/plastered_stone_wall_disp_1k.webp'),
     };
   }
 
   public static createLampTextures(): { [key: string]: Texture } {
     return {
-      color: loadTexture('../textures/lamp/wood_texture.webp', {
+      color: loadTexture('textures/lamp/wood_texture.webp', {
         colorSpace: true,
         magFilter: true,
       }),
